612a592126a27dfd226f941cc446b602f1521c1c
AlbertJager <kalbiban22@mail.ru>
2026-08-26 22:25:22 +0300

    final touches

--- app.py
+++ app.py
@@ -4,34 +4,24 @@ from pathlib import Path
 import argparse
 import dotenv
 import os
-"""
-Требования без изменений:
-
-Работает с одним username
-Корректно обрабатывает отсутствие пользователя
-Красивый JSON с отступами
-Код разбит минимум на функции
-README с инструкцией запуска
-Репозиторий на GitHub
-"""
 
 dotenv.load_dotenv()
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-def get_path():
-    '''Returns path to the script'''
-    path = Path(__file__).resolve().parent 
-    return path
 
 
 def get_userdata(username: str):
     '''Returns dictionary of user data'''
+    
+    if not GITHUB_TOKEN:
+        raise ValueError("GITHUB_TOKEN не найден. Проверь .env файл.")
+    
     g = (
         Github(auth=Auth.Token(GITHUB_TOKEN))
     )  # No authentication has limits: 60 requests per hour, 10 requests per minute
     try:
         user = g.get_user(username)
     except GithubException as e:
-        raise ValueError(f"Error {e.status}. User '{username}' not found.")
+        raise ValueError(f"Error {e.status}. {e.message}")
     else:
         user_data = {
             "login": user.login,
@@ -53,6 +43,7 @@ def get_userdata(username: str):
 
 def parser_engine():
     '''Creates parser and returns arguments'''
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("userlogin", type=str, help="Github userlogin")
     
@@ -62,19 +53,17 @@ def parser_engine():
 
 def write_json(path, user_data):
     '''Writes data to file'''
+    
     json_file = (path / f'{user_data["login"]}.json').resolve()
     
-    if json_file.parent == path:
-        with open(json_file, "w", encoding="utf-8") as file:
-            json.dump(user_data, file, indent=4, ensure_ascii=False)
-    else:
-        raise ValueError(fr"Invalid userlogin. Maybe it contains '\': {user_data["login"]}")    
-
+    with open(json_file, "w", encoding="utf-8") as file:
+        json.dump(user_data, file, indent=4, ensure_ascii=False)
+        print("The file was written successfully")
 
 def main():
     '''Main function'''
-    path = get_path()
-            
+    
+    path = Path(__file__).resolve().parent            
     args = parser_engine()
     user_data = get_userdata(args.userlogin)    
     write_json(path, user_data)
